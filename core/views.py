2ebcabd642954df871da6f0d24312a3ff6ed56a0
Sarah <sarah.l04@aluno.ifsc.edu.br>
2026-02-12 16:28:10 -0300

    Criação de página lista de contatos

--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
-    context = {'curso': 'Desenvolvimento de Sistemas', 'nome': 'Sarah', 'telefone': '(47)91234-5678', 'email': 'sarah@email.com'}
+    context = {'curso': 'Desenvolvimento de Sistemas'}
     return render(request, 'index.html', context)
+
+def contato(request):
+    context = { 
+        'nome': 'Sarah',
+        'telefone': '(47)91234-5678',
+        'email': 'sarah@email.com'
+    }
+    return render(request, 'contato.html', context)
