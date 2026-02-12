1814ff5c50d8eb99ca3d16225a5e3d0736f2680f
Sarah <sarah.l04@aluno.ifsc.edu.br>
2026-02-12 16:19:07 -0300

    Adição de lista de variáveis

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {'curso': 'Desenvolvimento de Sistemas', 'nome': 'Sarah', 'telefone': '(47)91234-5678', 'email': 'sarah@email.com'}
+    return render(request, 'index.html', context)
